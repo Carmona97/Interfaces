d86655fcbfafb8da715f3b3a417753d109a769af
Juanma Carmona <juanmacarmona97@gmail.com>
2024-03-07 19:59:21 +0100

    reloj analogico

---
+++ Proyectos/relojAnalogico/relojAnalogico/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using relojAnalogico.Componente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace relojAnalogico
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
